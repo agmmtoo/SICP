a46a8d0f9543cc432652c775d1629b93329876e9
Aung Myint Myat Oo <solidifyarmor@gmail.com>
2021-12-08 21:12:27 +0630

    1.2.1

---
+++ Ackermann.js
@@ -0,0 +1,5 @@
+function Ackermann(x, y) {
+    if (y === 0) return 0;
+    if (x === 0) return 2 * y;
+    if (y === 1) return 2;
+    return Ackermann(x-1, Ackermann(x, y-1))}
--- factorial-alt.scm
+++
@@ -1,8 +0,0 @@
-(define (factorial n) (fact-iter 1 1 n))
-
-(define (fact-iter result counter number)
-  (if (> counter number)
-    result
-    (fact-iter (* result counter) (+ counter 1) number)))
-
-(factorial 5)
